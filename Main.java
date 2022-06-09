599c1e3e9400590782b9d330cbc9fcfe3faff0a6
fco103 <fco10393@gmail.com>
2022-06-09 10:17:09 -0400

    Tema 3
    primera parte

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(sum(1,2,3));
+    }
+
+    public static int sum(int a, int b, int c) {
+
+        return a + b + c;
+
+    }
+}
